7c1aabd9a3fdf05c9d475db096b411cc32d64055
Fundiswa Tshabalala <102534208+Fundiswa-git@users.noreply.github.com>
2022-04-20 19:49:45 +0200

    Update task010.py

--- task010.py
+++ task010.py
@@ -1,8 +1,9 @@
-str1=input("enter string")
-str2=input("enter string")
-s1=set(str1)
-s2=set(str2)
-  print(s1,s2)
-com=s1 & s2
-  print(com)
+def common_letters(str1,str2):
+  common_letters=""
+  for char in set(str1):
+    if_char_in_str2:
+      common_letters+=char
+      print("common_letters:",",".join(common_letters).lower())
+
+      common_letters("England,English")
   
