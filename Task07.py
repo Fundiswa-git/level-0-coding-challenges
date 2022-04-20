1785d62ce07335e17fdcb617beb3ce83d5f2db8a
Fundiswa Tshabalala <102534208+Fundiswa-git@users.noreply.github.com>
2022-04-20 19:46:26 +0200

    Update Task07.py

--- Task07.py
+++ Task07.py
@@ -1,6 +1,16 @@
-(c * 1.8) +32
-  =Fahrenheit
+def convert_to_fahrenheit(celsius)
+fahrenheit=(celsius*9/5)+32
+return(fahrenheit)
+convert_to_fahrenheit=(convert_to_fahrenheit(37))
 
-  Celsius=0
-Fahrenheit = (Celsius*1.8)+32
-print("%.2F Celsius=% 2F Fahrenheit") %(Celsius,Fahrenheit)
+print(convert_to_fahrenheit)
+
+
+def convert_to_celsius(fahrenheit)
+celsius=(fahrenheit)
+return(celsius)
+convert_to_celsius=(convert_to_celsius(37))
+
+print(convert_to_celsius)
+
+convert_fahrenheit_to_celsius
