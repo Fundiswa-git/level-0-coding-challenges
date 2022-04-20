678e01f9d833a8771a376fe87b2d80dccf0c12e8
Fundiswa Tshabalala <102534208+Fundiswa-git@users.noreply.github.com>
2022-04-20 19:56:52 +0200

    Update task05.py

--- task05.py
+++ task05.py
@@ -1,4 +1,12 @@
-a**2 + b**2 = c**2
-area = 1/2(b)(h)
-area = 1/2(a)(b)
-print(area)
+def area(s1,s2,s3)
+s_perimeter=1/2*(s1+s2+s3)
+s_perimeter_m_s1=s_perimeter-s1
+
+s_perimeter_m_s2=s_perimeter-s2
+s_perimeter_m_s3=s_perimeter-s3
+
+area=(s_perimeter)*(s_perimeter_m_s1)*(s_perimeter_m_s2)*(s_perimeter_m_s3)
+
+return sqrt(area)
+
+print(area(1,2,3))
