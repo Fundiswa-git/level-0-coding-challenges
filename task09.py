50d82f0a15fbcaba635bd0aec7074d3d371d9505
Fundiswa Tshabalala <102534208+Fundiswa-git@users.noreply.github.com>
2022-04-20 20:05:59 +0200

    Update task09.py

--- task09.py
+++ task09.py
@@ -1,17 +1,6 @@
-name = ("enter a word")
-if 'a' in name or 'e' in name or 'i' in name or 'o' in name or 'u' in name
-print('vowel present in '+name)
-     else:
-print(no vowel present in '+name)
+def vowel_finder(user_string):
+    vowels=set([each_for_each_in_user_string_if_each_in_"aeiouAEIOU"])
+      print("vowels_found_in_a_string:",",".join(vowels).lower())
 
-if "a" in name:
-    print('a')
-if "e" in name:
-    print('e')
-if "i" in name:
-    print('i')
-if "o" in name:
-    print('o')
-if "u" in name:
-    print('u')
+      vowels_finder("Umuzi")
     
