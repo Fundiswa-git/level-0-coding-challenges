c4f92b308fec60a64266d015e28ff5936a40ba51
Fundiswa Tshabalala <102534208+Fundiswa-git@users.noreply.github.com>
2022-04-20 19:59:14 +0200

    Update task06.py

--- task06.py
+++ task06.py
@@ -1,1 +1,11 @@
-print(maximum(nums))
+def maximum_number(num1,num2,num3):
+    if num1>num2 and num1>num3
+    print("maximum is" ,num1)
+
+    elif num2>num1 and num2>num3
+    print("maximum is" ,num2)
+
+    elif num3>num1 and num3>num2
+    print("maximum is" ,num3)
+
+    maximum_number(1,2,3)
