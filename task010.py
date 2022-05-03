b701c0639ece0860b11486bdd4adf39a6c929bd6
Fundiswa Tshabalala <102534208+Fundiswa-git@users.noreply.github.com>
2022-05-03 15:16:42 +0200

    Delete task010.py

--- task010.py
+++
@@ -1,9 +0,0 @@
-def common_letters(str1,str2):
-  common_letters=""
-  for char in set(str1):
-    if_char_in_str2:
-      common_letters+=char
-      print("common_letters:",",".join(common_letters).lower())
-
-      common_letters("England,English")
-  
